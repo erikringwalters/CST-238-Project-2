437baf301545b0830e4875ab66d23e3bf5c11181
Erik <erik.ring.walters@gmail.com>
2016-10-28 09:51:51 -0700

    Added name, title, abstract, course, date, and ID

--- Project 2_Ring-Walters/Project 2_Ring-Walters/LinkedList.h
+++ Project 2_Ring-Walters/Project 2_Ring-Walters/LinkedList.h
@@ -1,3 +1,10 @@
+//Erik Ring-Walters
+//October 28, 2016
+//CST 238 Data Structures
+//Project 2
+//Linked List Project
+//ID: 002488407
+
 //----- LinkedList.h -----
 #ifndef LINKEDLIST
 #define LINKEDLIST
---
+++ Project 2_Ring-Walters/Project 2_Ring-Walters/driver.cpp
@@ -0,0 +1,6 @@
+//Erik Ring-Walters
+//October 28, 2016
+//CST 238 Data Structures
+//Project 2
+//Linked List Project
+//ID: 002488407
